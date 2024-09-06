70a7a517a085bca9abb59a74e20c8135909abb91
abdullah11137 <javediqbal123999@gmail.com>
2024-09-06 18:38:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,275 @@
+// var firstName = "Abdullah";
+// var lastName = "Kaimkhani";
+// console.log(firstName + " " + lastName);
+
+// var fullName = "Abdullah Kaimkhani";
+// var city = "Karachi";
+// var education = "Bachelor";
+
+// console.log("Hello my name is "+ fullName +". I live in "+ city + " and my education is "+ education +"."  );
+
+// var n1134 = "KK";
+
+// Chapter 1: Alerts
+
+// Q1
+
+// alert("Welcome");
+
+// Q2
+
+// alert("Error! Please enter a valid password.")
+
+// Q3
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Q4
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Q5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Chapter2: Variable for Strings
+
+// Q1
+
+// var username;
+
+// Q2
+
+// var myName = "Abdullah Kaimkhani";
+
+// Q3
+
+// var message = "Hello World";
+// alert(message);
+
+// Q4
+
+// var studentName = "Abdullah Kaimkhani", studentAge = "15 years old", studentQualification = "Certified Mobile Application Development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentQualification);
+
+// Q5
+
+// var name = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(name);
+
+// Q6
+
+// var email = "abdullahjavedkk@gmail.com";
+// alert("My email address is " +email)
+
+// Q7
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// Q8
+
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// Q9
+
+// var symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert(symbols);
+
+
+// Chapter 3: Variable for Numbers
+
+
+// Q1
+
+// var age = 15;
+// alert("I am "+ age + " years old");
+
+
+// Q2
+
+// var numberOfVisits = 14;
+// alert("You have visited this site "+ numberOfVisits + " times.")
+
+
+// Q3
+
+
+// var birthYear = 2005;
+// document.write("My birth year is "+ birthYear + "<br> Data type of my declared variable is " + typeof birthYear);
+
+// Q4
+
+
+// var visitorName = "Abdullah Kaimkhani", productTitle = "T-shirts", Quantity = 5;
+// document.write(visitorName, " ordered "+ Quantity, " ", productTitle + " on XYZ Clothing store." )
+
+
+
+// Chapter 4: Variable Names: Legal & Illegal
+
+
+
+// Q1
+
+
+// var ali, ahmed, abdullah;
+
+
+
+// Q2
+
+
+// var ali123, abdullah, $ali$, ali_ahmed;
+
+
+// var 123ali, ahmed#;
+
+
+// Q3
+
+
+// document.write("<b>Rules for naming JS variables</b> <br> <br><br><br><br><br>");
+
+
+// document.write("Variable names can only contain letters, numbers, underscore (_) and dollar signs ($). For example $my_1stVariable <br>");
+
+// document.write("Variables must begin with a letter, underscore (_) or dollar sign ($). For example $name, _name or name <br>")
+
+// document.write("Variable names are case sensitive. <br>")
+
+// document.write("Variable names should not be JS keywords.")
+
+
+
+
+// Chapter 1: Alerts
+
+
+
+// Q1
+
+
+// var firstName = "Abdullah", lastName = "Kaimkhani", email = "abdullahjavedkk@gmail.com", phoneNumber = "03068383991", password = "******";
+
+// alert("First Name: "+ firstName);
+
+// alert("Last Name: "+ lastName);
+
+// alert("Email: "+ email);
+
+// alert("Phone Number: "+ phoneNumber);
+
+// alert("Password: "+password);
+
+
+
+// Q2
+
+
+// alert("You're learning JavaScript!");
+
+
+// Q3
+
+
+
+// alert("How're you doing!");
+
+
+
+// Chapter 2: Variables for string
+
+
+
+// Q1
+
+
+// var fullName;
+
+
+
+// Q2
+
+
+// var fullName;
+
+// fullName = "Abdullah Kaimkhani";
+
+
+// Q3
+
+
+
+// var teamName = "Alpha11";
+
+// alert("Team name: "+ teamName);
+
+
+
+// Q4
+
+
+
+// var bestMan = "Charlie";
+
+// bestMan = "Bravo";
+
+
+
+// Chapter 3 : Variables for Numbers
+
+
+
+// Q1 & Q2
+
+
+// var caseQty;
+
+// caseQty = 144;
+
+
+// Q3
+
+
+// var num = 9;
+
+
+
+// Q4
+
+
+
+// var sum;
+
+// sum = 5 + 10;
+
+
+
+// Q5
+
+
+// var merchTotal = 100;
+
+// var shippingCharge = 10;
+
+// var orderTotal = merchTotal + shippingCharge;
+
+// document.write(orderTotal);
+
+
+
+// Q6
+
+
+var name = "Abdullah";
+
+name = "Abdullah123";
+
+
